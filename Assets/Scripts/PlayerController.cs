acda43c6bc9a40c3f1db3e75d2e99c018512b636
Jasmine Li <jasmineli106@gmail.com>
2024-07-20 16:14:55 -0400

    player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [HideInInspector] public Vector2 position = Vector2.zero;
+    [HideInInspector] public float speed = 2;
+
     void Update()
     {
-        if()
+        if(Input.GetKeyDown(KeyCode.W)) {
+            position.y += speed;
+        }
+        if(Input.GetKeyDown(KeyCode.S)) {
+            position.y -= speed;
+        }
+        if(Input.GetKeyDown(KeyCode.D)) {
+            position.x += speed;
+        }
+        if(Input.GetKeyDown(KeyCode.A)) {
+            position.x -= speed;
+        }
+        transform.position = position;
     }
 
 
